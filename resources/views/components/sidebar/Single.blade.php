537ffdd0236ccd2d3c106b67034ad89bce66446c
dipta harimbawa <vipalkuda@gmail.com>
2023-01-29 02:12:25 +0700

    [fix] active link button sidebar

--- resources/views/components/sidebar/Single.blade.php
+++ resources/views/components/sidebar/Single.blade.php
@@ -1,10 +1,8 @@
-{{-- <x-sidebarS icons="" title="" href=""> --}}
-
-    <li class="nav-item">
-        <a href="{{$href}}" class="nav-link">
-            <span class="sidebar-icon">
-                <i class="{{ $icons }} me-2"></i>
-            </span>
-            <span class="sidebar-text">{{ $title }}</span>
-        </a>
-    </li>
+<li class="nav-item {{ $currentsite ? 'active' : '' }}">
+    <a href="{{$href}}" class="nav-link">
+        <span class="sidebar-icon">
+            <i class="{{ $icons }} me-2"></i>
+        </span>
+        <span class="sidebar-text">{{ $title }}</span>
+    </a>
+</li>
