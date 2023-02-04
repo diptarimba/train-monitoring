b3a8c111f86f7c07e6f410d3159f99ee81f83bd9
dipta harimbawa <vipalkuda@gmail.com>
2023-02-05 02:44:20 +0700

    [fix] image error view

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -148,7 +148,7 @@
           <li class="nav-item dropdown ms-lg-3">
             <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <div class="media d-flex align-items-center">
-                <img class="avatar rounded-circle" alt="Image placeholder" src="{{ '/' . Auth::user()->avatar}}">
+                <img class="avatar rounded-circle" alt="Image placeholder" src="{{ Auth::user()->avatar}}">
                 <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
                   <span class="mb-0 font-small fw-bold text-gray-900">{!! Auth::user()->name !!}</span>
                 </div>
